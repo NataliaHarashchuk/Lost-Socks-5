1c9aadef27bfb98cfc29169618c00f9e9ab68cd0
NataliaHarashchuk <159415965+NataliaHarashchuk@users.noreply.github.com>
2024-12-05 09:34:51 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -46,7 +46,7 @@ function getCookie(name) {
 }
 
 function eraseCookie(name) {
-    document.cookie = name + '=; Max-Age=-99999999;';  
+    document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
 }
 
 window.onload = function() {
